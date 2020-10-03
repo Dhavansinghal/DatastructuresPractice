62080d78bd323c555ac385d85a82c954332800c8
DhavanSinghal <dhavansinghal@gmail.com>
2020-10-03 14:59:03 +0530

    creating Stack

--- DSClass/Stack.py
+++ DSClass/Stack.py
@@ -1,5 +1,16 @@
 class Stack{
-    def def __init__(self):
-      self.container = list();
+    def __init__(self,size = -1):
+      (size == -1) ? self.container = [] : self.container = [None]*size
+      self.size = size
+      self.top = -1
+
+    def push(nextData):
+        if (self.top == self.size) or (self.size == 0):
+            print("Stackoverflow: Stack is full")
+        else:
+            self.container.append(nextData);
+            self.top += 1
+    
+    
 
 }
